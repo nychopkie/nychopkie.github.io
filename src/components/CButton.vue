dc409ce06a8ce6697b14a855b12835396aac73ca
nychopkie <annie030103@gmail.com>
2025-09-10 17:55:49 +0800

    updated README

---
+++ src/components/CButton.vue
@@ -0,0 +1,36 @@
+<template>
+    <button class="btn">
+        <slot>
+            test test 
+        </slot>
+    </button>
+</template>
+
+<style scoped>
+.btn {
+    background-color: #42b983;
+    border: none;
+    color: white;
+    padding: 10px 20px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+    border-radius: 8px;
+}
+.btn:hover {
+    background-color: #369870;
+}
+.btn:active {
+    background-color: #2c6e5c;
+}
+.btn:focus {
+    outline: none;
+}
+.btn:disabled {
+    background-color: #cccccc;
+    cursor: not-allowed;
+}
+</style>
