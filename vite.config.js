2f0debb4b702bad753f3e007319697c6655738fd
nychopkie <annie030103@gmail.com>
2025-10-21 23:51:31 +0800

    ????

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '', // Replace <REPONAME> with your actual repository name,
+  base: '/nychopkie.github.io/', // Replace <REPONAME> with your actual repository name,
   plugins: [
     vue(),
     tailwindcss(),
